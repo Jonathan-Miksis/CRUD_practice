7b0778c854f59c1a5d7ccd26263aab2b63a7cc39
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-16 14:13:43 -0400

    create patch route

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,4 +20,14 @@ class PhotosController < ApplicationController
     render json: new_photo
   end
 
+  def update
+    id = params[:id]
+    update_photo = Photo.find_by(id: id)
+    update_photo.name = params[:input_name] || update_photo.name
+    update_photo.width = params[:input_width] || update_photo.width
+    update_photo.height = params[:input_height] || update_photo.height
+    update_photo.save
+    render json: update_photo
+  end
+
 end
